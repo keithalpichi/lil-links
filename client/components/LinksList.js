a99d7bb0c7e81516bd324068da7c97fdd8b1925e
Keith Alpichi <keithalpichi@gmail.com>
2017-06-06 12:30:54 -0700

    added react-table styling and filterable/sortable functionality to link items

--- client/components/LinksList.js
+++ client/components/LinksList.js
@@ -1,8 +1,37 @@
 import React from 'react'
-import LinkItem from './LinkItem'
+import ReactTable from 'react-table'
+
+// const dateSearch = (filter, date) => {
+//   // (filter, row) => row[filter.id].includes(filter.value)
+// }
+
+const columns = [{
+  Header: 'Lil\' Link',
+  accessor: 'shortlink',
+  filterMethod: (filter, row) => row[filter.id].includes(filter.value)
+}, {
+  Header: 'Original URL',
+  accessor: 'url',
+  filterMethod: (filter, row) => row[filter.id].includes(filter.value)
+}, {
+  Header: 'Created',
+  accessor: 'createdat',
+  Cell: d => <span>{new Date(d.value).toDateString()}</span>,
+  filterable: false
+  // filterMethod: dateSearch()
+}, {
+  Header: 'Visits',
+  accessor: 'visits',
+  filterMethod: (filter, row) => row[filter.id].toString() === filter.value
+}]
 
 export default ({ links }) => (
-  <div>
-    {links.map(link => <LinkItem {...link} key={link.shortLink} />)}
-  </div>
+  <ReactTable
+    className='-striped -highlight'
+    filterable
+    resizable
+    data={links}
+    columns={columns}
+    defaultSorted={[{ id: 'createdat', desc: true }]}
+  />
 )
