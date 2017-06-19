1e81bdd7a54ca45a71584789e80b712e0ebf27d8
Keith Alpichi <keithalpichi@gmail.com>
2017-06-18 22:29:41 -0700

    remove host

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/:shortLink', shortLinkRouter)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
 
 if (!module.parent) {
-  app.listen(PORT, HOST, () => {
+  app.listen(PORT, () => {
     console.log(`Listening at: ${HOST}:${PORT} in ${ENV} environment`)
   })
 }
